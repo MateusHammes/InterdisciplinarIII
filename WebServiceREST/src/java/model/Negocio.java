9f5f0adb56f98e7d8a0940fc32dc8b1020f50d6e
MaxNaegele <maxnaegele2010@gmail.com>
2015-10-24 19:20:03 -0200

    Novas models

--- WebServiceREST/src/java/model/Negocio.java
+++ WebServiceREST/src/java/model/Negocio.java
@@ -1,10 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package model;
 
+import java.io.Serializable;
 import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -15,18 +12,15 @@ import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.xml.bind.annotation.XmlRootElement;
 
-/**
- *
- * @author mateus
- */
 @Entity
 @XmlRootElement
-public class Negocio {
+public class Negocio implements Serializable{
 
     @Id
     @SequenceGenerator(name = "neg_codigo", sequenceName = "seq_neg_codigo")
     @GeneratedValue(strategy = GenerationType.AUTO, generator = "neg_codigo")
     private int neg_codigo;
+    private int neg_codigoParent;
     private String neg_vnome;
     private Date neg_dcadastro;
     private char neg_cstatus;
@@ -34,6 +28,7 @@ public class Negocio {
     private Date neg_dtermino;
     private String neg_vendereco;
     private String neg_vdescricao;
+    private char neg_ctipo;
 
 //   @ManyToOne
 //    @JoinColumn(name = "pes_codigo", referencedColumnName = "pes_codigo")
