24e9e891239accdb5ea4a7c3a8bac1638db9e547
MaxNaegele <maxnaegele2010@gmail.com>
2015-10-24 19:24:22 -0200

    Ajustes de recursos

--- WebServiceREST/src/java/recursos/MateriaisResource.java
+++ WebServiceREST/src/java/recursos/MateriaisResource.java
@@ -3,11 +3,8 @@ package recursos;
 
 import dao.MateriaisDAO;
 import java.util.List;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -16,14 +13,11 @@ import javax.ws.rs.core.MediaType;
 import model.Materiais;
 
 
-@Path("materiais")
+@Path("/materiais")
 public class MateriaisResource {
 
     MateriaisDAO materiaisDAO = new MateriaisDAO();
-    @Context
-    private UriInfo context;
 
-    
     public MateriaisResource() {
     }
 
