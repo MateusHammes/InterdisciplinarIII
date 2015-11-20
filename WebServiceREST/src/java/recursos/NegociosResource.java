45065c88bd152156935afbef32e39947c90ab1b2
MaxNaegele <maxnaegele2010@gmail.com>
2015-11-19 22:05:22 -0200

    Novo método

--- WebServiceREST/src/java/recursos/NegociosResource.java
+++ WebServiceREST/src/java/recursos/NegociosResource.java
@@ -3,20 +3,23 @@ package recursos;
 import Enum.NegocioStatus;
 import Enum.NegocioTipo;
 import dao.NegocioDAO;
+import dao.ProdutoDAO;
+import dao.ProdutoMaterialDAO;
+import dao.RegistrosDAO;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import model.Grupo;
 import model.Negocio;
+import model.Produto;
+import model.ProdutoMaterial;
+import model.Registros;
 
 @Path("/negocios")
 public class NegociosResource {
@@ -98,15 +101,63 @@ public class NegociosResource {
             neg.setNeg_vendereco(orcamento.getNeg_vendereco());
             neg.setNeg_vnome(orcamento.getNeg_vnome());
             neg.setNeg_codigo(0);
-            negocioDAO.insert(neg);
 
+            ProdutoDAO pDAO = new ProdutoDAO();
+            ///pega todos os produtos do negocio
+            List<Produto> lsProdutos = pDAO.findAll(neg.getNeg_codigo());
+
+            negocioDAO.insert(neg);
             orcamento.setNeg_cstatus(NegocioStatus.Concluido);
             negocioDAO.update(orcamento);
+
+            /// List<Produto> lsProdutosNew = new ArrayList<>();
+            for (Produto p : lsProdutos) {
+                CreateProduto(p, neg);
+                ///lsProdutosNew.add(p);
+            }
+            ///seta a lista de produtos no negocio
+
             return neg.getNeg_codigo() + "";
 
         } catch (Exception e) {
         }
         return "0";
+    }
+
+    private Produto CreateProduto(Produto p, Negocio neg) {
+        Produto pNew = new Produto();
+        pNew.setPro_vnome(p.getPro_vnome());
+        pNew.setPro_ctipo(p.getPro_ctipo());
+        pNew.setPro_vdescricao(p.getPro_vdescricao());
+        pNew.setNegocio(neg);
+
+        RegistrosDAO rDAO = new RegistrosDAO();
+        List<Registros> lsRegistros = rDAO.findByIdProd(p.getPro_codigo());
+        List<Registros> lsRegistrosNew = new ArrayList<>();
+        for (Registros reg : lsRegistros) {
+            Registros rg = new Registros();
+            rg.setRgs_cstatus(reg.getRgs_cstatus());
+            rg.setRgs_vdescricao(reg.getRgs_vdescricao());
+            rg.setProduto(pNew);
+            rDAO.insert(rg);
+            //lsRegistrosNew.add(rg);
+        }
+        ProdutoMaterialDAO pmDAO = new ProdutoMaterialDAO();
+        List<ProdutoMaterial> lsPM = pmDAO.findAll(p.getPro_codigo(), 0);
+        List<ProdutoMaterial> lsPmNew = new ArrayList<>();
+
+        for (ProdutoMaterial pm : lsPM) {
+            ProdutoMaterial pmNew = new ProdutoMaterial();
+            pmNew.setMaterial(pm.getMaterial());
+            pmNew.setPrm_iunidade(pm.getPrm_iunidade());
+            pmNew.setPrm_iunidadeUtilizada(pm.getPrm_iunidadeUtilizada());
+            pmNew.setPrm_nvalor(pm.getPrm_nvalor());
+            pmNew.setProduto(pNew);
+            pmDAO.insert(pmNew);
+            /// lsPmNew.add(pmNew);
+        }
+
+        return pNew;
     }
 
 }
